aa36094478e0aa2e12f8f0ace21570169c4effa5
Sasayaki7 <rikurikuron3@hotmail.com>
2021-09-29 22:51:14 -0400

    test3

--- flask_app/controllers/user_controllers.py
+++ flask_app/controllers/user_controllers.py
@@ -64,8 +64,8 @@ def login():
 
 @app.route('/fetch-sequence', methods=['POST'])
 def get_json():
-    path = pathlib.PurePath(__file__).parent.parent
     return "Record not found", 400
+    path = pathlib.PurePath(__file__).parent.parent
 
     path = path.joinpath('static/sequence-obj/')
     file = open(f'{path}{request.form["filename"]}')
